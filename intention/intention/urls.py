e52e72b84e002015ecfc57fefb3e19d1e5187ec8
NichelleBot <nichelle.a.hall@gmail.com>
2019-01-29 17:52:21 -0800

    logo is home screen

--- intention/intention/urls.py
+++ intention/intention/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from intention_app.views import logo_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', logo_view, name='logo')
 ]
--- intention/intention_app/views.py
+++ intention/intention_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+# A Sample View to Render CozyCo Logo 
+def logo_view(request):
+    image_data = open("intention_app/static/cozyco-logo.png", "rb").read()  
+    return HttpResponse(image_data, content_type="image/png")
